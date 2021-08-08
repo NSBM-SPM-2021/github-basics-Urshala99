faa308afb76a0381eea4d1b70c5fe1d53f51f852
Urshala99 <urshala2@gmail.com>
2021-08-08 21:51:19 +0530

    Files added

---
+++ tshop/src/components/Products/Products.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+const products = [
+    {id: 1, name: 'Tone', description: 'T shirt with heart', price: '$5'},
+    {id: 2, name: 'Ttwo', description: 'T shirt with orange', price: '$10'},
+];
+
+const Products = () => {
+<main>
+    <Grid container justify="center" spacing={4}>
+    {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+        </Grid>
+    ))}
+  </Grid>
+</main>
+}
+
+export default Products;
