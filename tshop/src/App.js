b8179fa492fa32747199a92bcf5c0a2b8effe96b
Urshala99 <urshala2@gmail.com>
2021-08-10 10:25:10 +0530

    Commerce products created

--- tshop/src/App.js
+++ tshop/src/App.js
@@ -6,6 +6,7 @@ import { Products, Navbar } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -14,15 +15,31 @@ const App = () => {
 
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+
+    }
+
+
     useEffect(() => {
-            fetchProducts();
+        fetchProducts();
+        fetchCart();
 
     }, []);
 
+    console.log(cart);
+
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems={cart.total_items} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
