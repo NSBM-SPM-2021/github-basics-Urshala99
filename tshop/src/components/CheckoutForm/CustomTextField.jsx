0e36bcc36092797aeb87ed4af7992e0d9eaaa3c1
Urshala99 <urshala2@gmail.com>
2021-08-16 21:56:23 +0530

    AddressForm initiated

--- tshop/src/components/CheckoutForm/CustomTextField.jsx
+++ tshop/src/components/CheckoutForm/CustomTextField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label}) => {
     const { control } = useFormContext();
     const isError = false;
     return (
@@ -15,7 +15,7 @@ const FormInput = ({ name, label, required }) => {
                     <TextField
                         fullWidth
                         label={label}
-                        required={required}
+                        required
                     />
                 )}
             />
